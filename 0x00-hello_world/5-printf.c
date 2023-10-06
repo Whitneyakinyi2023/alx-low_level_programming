cb8527a8f3c02867ec678b38329fdda61ba86a75
Whitneyakinyi2023 <whitneyakinyi73@gmail.com>
2023-10-05 17:00:32 -0700

    print

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+
+/**
+ * main - prints with proper grammar, but the outcome is a piece of art,
+ * Return: Always 0.
+ */
+
+
+int main(void)
+
+
+{
+	
+	prints("\"prints with proper grammar, but the outcome is a piece of art,);
+	return 0);
+}
