426b9552f5e512f92a1521fff8a3a0e2fb093f0b
Whitneyakinyi2023 <whitneyakinyi73@gmail.com>
2023-10-09 04:28:21 -0700

    write

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,14 @@
 #include <unistd.h>
 /**
- * main - print without the print command
+ * main - prints "and that piece of art is useful" - Dora Korpar, 2015-10-19, followed by a new line, to the standard error
  * Description: Using write instead of print
  * Return: Always 1.
  */
 
+
 int main(void)
 
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
 	return (1);
 }
