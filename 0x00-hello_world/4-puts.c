ed329798771d80364fa0d8db448893f79674cfad
Whitneyakinyi2023 <whitneyakinyi73@gmail.com>
2023-10-09 03:27:13 -0700

    sizes

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,12 @@
 #include <stdio.h>
-
-
 /**
- *main - prints "Programming is like building a multilingual puzzle
- *Return: Always 0.
+ * main - prints "Programming os like building a multilingual puzzle, followed by a new line"
+ * return 0
  */
 
 int main(void)
 
 {
-	puts("\"Programming is like building a multilingual puzzle");
-
-	return (0);
+	puts ("\"Programming is like building a multilingual puzzle")
+		return (0)
 }
