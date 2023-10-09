19df405acb5af37f508edf0766d6430f634581a4
Whitneyakinyi2023 <whitneyakinyi73@gmail.com>
2023-10-09 03:33:01 -0700

    data types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - Prints the main types based on the computers it is compiled and run on..
+ * main - Prints the main types of data.
  * Return: Always 0.
  */
 
@@ -9,7 +9,7 @@ int main(void)
 {
 
 	printf("Size of a char: %lu  byte(s)\n", sizeof(char));
-	printf("Size of an int : %lu byte (s\n", sizeof(int));
+	printf("Size of an int: %lu byte (s\n", sizeof(int));
 	printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
 	printf("Size of a float: %lu byte(s)\n", sizeof(float));
