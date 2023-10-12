7dc29398f5191f61673bb1c8909d3b8fff8b6cbe
Whitneyakinyi2023 <whitneyakinyi73@gmail.com>
2023-10-12 12:17:52 -0700

    line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_line - Print line to straight
+ * @n: No of spaces
+ * return:0
+ */
+
+void print_line(int n)
+
+{
+int c;
+for (c = 0; c < n; c++)
+{
+	_putchar('_');
+}
+_putchar('\n');
+}
