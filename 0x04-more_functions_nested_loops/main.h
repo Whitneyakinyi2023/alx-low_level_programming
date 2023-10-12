17148af7bfdf8672a28fbd92f3e0223629564dcb
Whitneyakinyi2023 <whitneyakinyi73@gmail.com>
2023-10-11 22:08:26 -0700

    initial changes

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_HEADER_FILE
+#define MAIN_HEADER_FILE
+
+void print_number(int n);
+int _isupper(int c);
+void print_triangle(int size);
+void print_square(int size);
+void print_numbers(void);
+void print_diagonal(int n);
+void print_line(int n);
+void more_numbers(void);
+void print_most_numbers(void);
+int mul(int a, int b);
+int _isdigit(int c);
