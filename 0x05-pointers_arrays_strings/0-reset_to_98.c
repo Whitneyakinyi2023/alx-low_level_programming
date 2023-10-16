4e51dfc09f7d824f9674766ae2478924f0aac586
Whitneyakinyi2023 <whitneyakinyi73@gmail.com>
2023-10-16 05:11:49 -0700

    reset to 98

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,18 +1,12 @@
 #include "main.h"
 /**
- * void reset_to_98 - updates pointer to 98
+ * reset_to_98 - updates pointer to 98
  * @n :integer in question
  * Return: Always 0 (Successful)
  */
 
 void reset_to_98(int *n)
-{
-	int n;
-	int *p;
 
-	n = 98;
-	p = &n;
-	
-	_putchar("Address of 'n': %p\n", &n);
-	return (0);
+{
+	*n = 98;
 }
