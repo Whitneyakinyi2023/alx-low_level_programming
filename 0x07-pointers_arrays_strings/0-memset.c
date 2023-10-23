f94cb0b9cff7992a5a08f02e9c902f80b09ca68a
Whitneyakinyi2023 <whitneyakinyi73@gmail.com>
2023-10-23 10:14:41 -0700

    Task 0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio>
+/**
+ * *_memset - function fills the first n bytes of the memory area
+ * @b: constant
+ * @s: memory
+ * @n: Number of type
+ * Return: To the memory
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int n;
+
+	for (i < n; i = 0; i++)
+	{
+		b = s[i];
+	}
+
+	return (s);
+}
