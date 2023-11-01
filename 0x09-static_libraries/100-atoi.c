9410b9ef50ed67ee8b1b14062811873cd19e38bc
Whitneyakinyi2023 <whitneyakinyi73@gmail.com>
2023-11-01 15:03:57 -0700

    Task 2

--- 0x09-static_libraries/100-atoi.c
+++ 0x09-static_libraries/100-atoi.c
@@ -1,15 +1,7 @@
 #include "main.h"
 /**
  * _atoi - Function that turns string to integer
- * @s: String in question
  * Return: int
  */
 
-int _atoi(char *s)
-{
-	int x;
-	int w;
-	int y;
-	int z;
-	int len;
-}
+int _atoi(char *s);
