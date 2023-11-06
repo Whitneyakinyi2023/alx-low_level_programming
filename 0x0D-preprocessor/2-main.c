f53cd8d822822c55e4bded41461c4f6508ab99c1
Whitneyakinyi2023 <whitneyakinyi73@gmail.com>
2023-11-06 15:05:12 -0800

    task 4

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,6 +1,8 @@
 #include<stdio.h>
+
 /**
  * main - prints the name of the file it was compiled from
+ * @_FILE_: ......
  * Return: 0 successful
  */
 
@@ -8,5 +10,6 @@ int main(void)
 
 {
 	printf("%s\n", _FILE_);
+	
 	return (0);
 }
