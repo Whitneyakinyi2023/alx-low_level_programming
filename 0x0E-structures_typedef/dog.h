824273276ce686f6e8f15a94c1dfd71730db8ba5
Whitneyakinyi2023 <whitneyakinyi73@gmail.com>
2023-11-06 18:33:31 -0800

    rask 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - New type
+ * @name: name
+ * @age: Age
+ * @owner: owner
+ */
+
+struct dog
+{
+	char  * name;
+	char * owner;
+	float * age;
+}
+
+#endif
