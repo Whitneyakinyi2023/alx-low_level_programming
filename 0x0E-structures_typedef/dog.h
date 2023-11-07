d4009ab19f9442f319cad31dcfd2797302c37eb2
Whitneyakinyi2023 <whitneyakinyi73@gmail.com>
2023-11-06 19:14:19 -0800

    taheader

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -8,33 +8,22 @@
  * @owner: owner
  */
 
-struct dog;
+struct dog
+
 {
-	char  * name;
-	char * owner;
-	float * age;
-}
+	char  *name;
+	char *owner;
+	float *age;
+};
+
+/**
+ * dog_t - Typedef for dog structure
+ */
+type def struct dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 
-#ifndef DOG_H
-#define DOG_H
-
-/**
- * struct dog - New type
- * @name: name
- * @age: Age
- * @owner: owner
- */
-struct dog;
-{
-        char  * name;
-        char * owner;
-        float * age;
-}
-
-type def struct dog_t;
 #endif
