8977df8ed375cf201d1b96bdec718a46ed4515e6
Whitneyakinyi2023 <whitneyakinyi73@gmail.com>
2023-11-07 00:47:43 -0800

    0.POPPY

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -13,13 +13,15 @@ struct dog
 {
 	char  *name;
 	char *owner;
-	float *age;
+	float age;
 };
 
 /**
- * dog_t - Typedef for dog structure
+ * struct - ..
+ * @dog_t - Typedef for dog structure
+ *
  */
-type def struct dog_t;
+typedef struct dog  dog_t ;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
