13ce3f3797d676222d38422a3becefa145523003
Whitneyakinyi2023 <whitneyakinyi73@gmail.com>
2023-11-19 14:06:40 -0800

    new

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
+
 /**
- * main - prints "Programming os like building a multilingual puzzle, followed by a new line"
+ * main - printsnew line"
  * Return: 0
  */
 
 int main(void)
 
 {
-	puts ("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
