8ad50d2e1e6adcf73837b67f40e8053b98c82416
Whitneyakinyi2023 <whitneyakinyi73@gmail.com>
2023-11-24 18:10:19 -0800

    task 1 correction

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -5,8 +5,27 @@
  * @n:..
  * Return:Num
  */
+void print_bi(unsigned long int n)
+{
+	if (n == 0)
+		return;
+	print_bi(n >> 1);
+	if ((n & 1) == 1)
+		_putchar('1');
+	if ((n & 0) == 0)
+		_putchar('0');
+}
+/**
+ * print_binary - Prints binary representation of a number
+ * @n:..
+ * Return:Num
+ */
 void print_binary(unsigned long int n)
 {
-	int num;
-
+	if (n == 0)
+		_putchar('0');
+	else
+	{
+		print_bi(n);
+	}
 }
