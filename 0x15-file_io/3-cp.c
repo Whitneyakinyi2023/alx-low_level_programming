597ef4d1bd43c202f448266d368cb1e1322c9076
Whitneyakinyi2023 <whitneyakinyi73@gmail.com>
2023-12-01 18:56:10 -0800

    tak 2

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -4,10 +4,35 @@
 #include "main.h"
 #include <unistd.h>
 #include <fcntl.h>
+#define BUF_SIZE 1024
 #include <sys/stat.h>
 /**
- * create_file - function that appends text at the end of a file.
- * @filename: the name of the file
- * @text_content:the NULL terminated string to add at the end of the file
- * Return: 1 on success and -1 on failure
+ * main - function that program that copies the content of a file to another file..
+ * @argc: the number of arguments
+ * @argv: pointer
+ * Return: 0
  */
+
+int main(int argc, char **argv)
+{
+	int file_from;
+	int r1;
+	int r2;
+	int file_to;
+	char *buffer;
+
+	if (argc != 3)
+	{
+		dprintf(STDERR, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+	buffer = malloc(sizeof(char) * BUF_SIZE);
+	if (!buffer)
+		return (0);
+	file_from = open(argv[1], O-RDONLY);
+	error_98 = (file_from, buffer, argv[1]);
+	file_to = open(argv[2], O_WRONLY | O_TRUNC | O_CREAT, 0664);
+	error_99 = (file_tp, buffer, argv[2]);
+	return(0);
+
+}
