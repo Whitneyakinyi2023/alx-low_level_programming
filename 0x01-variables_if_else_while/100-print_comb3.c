dcbc9d4b025c3823c24c96945583785b2dee3d5e
Whitneyakinyi2023 <whitneyakinyi73@gmail.com>
2023-12-06 01:15:08 -0800

    With inclusionon of + '0' for ASCII

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,25 +1,24 @@
 #include <stdio.h>
 /**
  * main - entry point
- * Description : 'output of possible singke digit combinations'
+ * Description: 'output of possible singke digit combinations'
  * Return: Always 0
  */
 
 int main(void)
 {
-
 	int t;
 	int x;
 
-	for (t = '0'; t <=  8; t++)
+	for (t = 0; t <=  8; t++)
 	{
-		for (x = t + 1; x <= '9'; x++)
+		for (x = t + 1; x <= 9; x++)
 		{
 
 			{
-				putchar(x);
-				putchar(t);
-				if  (x != '8' ||  t != '9')
+				putchar(x + '0');
+				putchar(t + '0');
+				if  (x != 8 || t != 9)
 				{
 				putchar(',');
 				putchar(' ');
