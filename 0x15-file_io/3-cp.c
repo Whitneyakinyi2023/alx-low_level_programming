5328b6e52135d26442b4a9a22621fc1a24db8291
Whitneyakinyi2023 <whitneyakinyi73@gmail.com>
2023-12-08 23:53:01 -0800

    cp file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#include "main.h"
 #include <unistd.h>
+#include "main.h"
 #include <fcntl.h>
 #define BUF_SIZE 1024
 #include <sys/stat.h>
@@ -23,16 +23,32 @@ int main(int argc, char **argv)
 
 	if (argc != 3)
 	{
-		dprintf(STDERR, "Usage: cp file_from file_to\n");
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
 	buffer = malloc(sizeof(char) * BUF_SIZE);
 	if (!buffer)
 		return (0);
-	file_from = open(argv[1], O-RDONLY);
-	error_98 = (file_from, buffer, argv[1]);
+	file_from = open(argv[1], O_RDONLY);
+	error_98(file_from, buffer, argv[1]);
 	file_to = open(argv[2], O_WRONLY | O_TRUNC | O_CREAT, 0664);
-	error_99 = (file_tp, buffer, argv[2]);
-	return(0);
+	error_99(file_to, buffer, argv[2]);
+
+	do
+	{
+		r1 = read(file_from, buffer, argv[1]);
+		if  (r1 == 0)
+			break;
+		error_98(r1, buffer, argv[1]);
+		r2 = write (file_from, buffer, r1);
+		error_99(r1, buffer, argv[2]);
+	}
+	while (r2 >= BUF_SIZE);
+	r1 = close(file_from);
+	error_100(r1, buffer);
+	r1 = close(file_to);
+	error_100(r1, buffer);
+	free(buffer);
+	return (0);
 
 }
